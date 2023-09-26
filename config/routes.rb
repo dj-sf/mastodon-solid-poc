6a2bc4162cde06f46ce4a8d7d31ceb577c782c67
djsf <30304653+dj-sf@users.noreply.github.com>
2023-09-26 12:47:12 +0000

    login with inrupt works but callback fails

--- config/routes.rb
+++ config/routes.rb
@@ -186,6 +186,8 @@ Rails.application.routes.draw do
   get '/privacy-policy', to: 'privacy#show', as: :privacy_policy
   get '/terms',          to: redirect('/privacy-policy')
 
+  # get '/callback',   to:
+
   match '/', via: [:post, :put, :patch, :delete], to: 'application#raise_not_found', format: false
   match '*unmatched_route', via: :all, to: 'application#raise_not_found', format: false
 end
